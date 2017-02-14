64c0be3bacd1f884f04c24b5ae7ff875529e4e8d
Robbie Cao <robbie.cao@gmail.com>
2017-02-14 16:14:35 +0800

    Follow up f2d3658, Map flash bank with constants into XDATA
    Signed-off-by: Robbie Cao <robbie.cao@gmail.com>

--- SDK/CC254X/Components/hal/target/CC2540EB/hal_startup.c
+++ SDK/CC254X/Components/hal/target/CC2540EB/hal_startup.c
@@ -86,9 +86,9 @@ __low_level_init(void)
   /*==================================*/
   // Map flash bank with constants into XDATA for access to "ROM mapped as data".
 #if defined HAL_IMAGE_A
-  MEMCTR = (MEMCTR & 0xF8) | 0x05;
+  MEMCTR = (MEMCTR & 0xF8) | 0x06;
 #elif defined HAL_IMAGE_B
-  MEMCTR = (MEMCTR & 0xF8) | 0x04;
+  MEMCTR = (MEMCTR & 0xF8) | 0x05;
 #else
   MEMCTR = (MEMCTR & 0xF8) | 0x01;
 #endif
